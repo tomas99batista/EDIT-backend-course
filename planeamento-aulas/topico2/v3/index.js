5d0e45a14a3d2adc8c72282e783286856884c90e
Tomás Batista <34144094+tomas99batista@users.noreply.github.com>
2024-07-04 17:09:52 +0100

    refactor(structure): folders

--- planeamento-aulas/topico2/v3/index.js
+++ planeamento-aulas/topico2/v3/index.js
@@ -6,6 +6,12 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+app.get("/posts/:id", (req, res) => {
+  const id = req.params.id;
+  console.log();
+  res.send(`Post Id: ${id}`);
+});
+
 app.listen(process.env.PORT, () => {
   console.log("Server running");
 });
